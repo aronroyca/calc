63c9e6e5e1570f36cf7b86705618a8ab6800a5a8
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2022-12-23 21:17:50 -0800

    Added a default intruction section when the comand calc is run with no arguments and updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # calc
-this is a command line calculator
+This is a command line calculator:
+
+    Flag 1 - Supported operations
+      -add -subtract -multiply -divide
+      
+    Flag 2 and 3 are numbers to be operated on
+
+     ex. calc multiply 2 3
+        --outputs 6
--- bin/index.js
+++ bin/index.js
@@ -2,8 +2,20 @@
 const yargs = require('yargs');
 
 var arguments = process.argv;
-console.log(arguments);
-if (arguments[2].toLowerCase() === 'add') {
+
+// console.log(arguments);
+if (arguments.length === 2) {
+    console.log(`
+    This is a command line calculator:
+
+    Flag 1 - Supported operations
+      -add -subtract -multiply -divide
+      
+    Flag 2 and 3 are numbers to be operated on
+
+     ex. calc multiply 2 3
+        --outputs 6`)
+} else if (arguments[2].toLowerCase() === 'add') {
     console.log(parseInt(arguments[3]) + parseInt(arguments[4]));
 } else if (arguments[2].toLowerCase() === 'subtract') {
     console.log(parseInt(arguments[3]) - parseInt(arguments[4]));
