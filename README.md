5ecb6091c6450db3883f7614fce9657848947d45
Aron Kumar Aroy <113210093+aronroyca@users.noreply.github.com>
2023-01-25 13:58:38 -0800

    added exponents

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # calc
+
 This is a command line calculator:
 
     Flag 1 - Supported operations
-      -add -subtract -multiply -divide
-      
+      -add -subtract -multiply -divide -exponent
+
     Flag 2 and 3 are numbers to be operated on
 
      ex. calc multiply 2 3
--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
+const yargs = require("yargs");
 
 var arguments = process.argv;
 
 // console.log(arguments);
 if (arguments.length === 2) {
-    console.log(`
+  console.log(`
     This is a command line calculator:
 
     Flag 1 - Supported operations
@@ -14,18 +14,17 @@ if (arguments.length === 2) {
     Flag 2 and 3 are numbers to be operated on
 
      ex. calc multiply 2 3
-        --outputs 6`)
-} else if (arguments[2].toLowerCase() === 'add') {
-    console.log(parseInt(arguments[3]) + parseInt(arguments[4]));
-} else if (arguments[2].toLowerCase() === 'subtract') {
-    console.log(parseInt(arguments[3]) - parseInt(arguments[4]));
-} else if (arguments[2].toLowerCase() === 'multiply') {
-    console.log(parseInt(arguments[3]) *  parseInt(arguments[4]));
-} else if (arguments[2].toLowerCase() === 'divide') {
-    console.log(parseInt(arguments[3]) / parseInt(arguments[4]));
+        --outputs 6`);
+} else if (arguments[2].toLowerCase() === "add") {
+  console.log(parseInt(arguments[3]) + parseInt(arguments[4]));
+} else if (arguments[2].toLowerCase() === "subtract") {
+  console.log(parseInt(arguments[3]) - parseInt(arguments[4]));
+} else if (arguments[2].toLowerCase() === "multiply") {
+  console.log(parseInt(arguments[3]) * parseInt(arguments[4]));
+} else if (arguments[2].toLowerCase() === "divide") {
+  console.log(parseInt(arguments[3]) / parseInt(arguments[4]));
+} else if (arguments[2].toLowerCase() === "exponent") {
+  console.log(parseInt(arguments[3]) ** parseInt(arguments[4]));
 } else {
-    console.log(`${arguments[2]} operation not recognized`)
+  console.log(`${arguments[2]} operation not recognized`);
 }
-
-
-
